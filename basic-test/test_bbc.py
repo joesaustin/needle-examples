b809179a06ac4a5b7cd7a96104bc8dbcc3f4cb73
Russell Bradley <me@russellbradley.com>
2014-11-09 23:44:45 -0500

    - add os, and use env var for url

--- basic-test/test_bbc.py
+++ basic-test/test_bbc.py
@@ -1,9 +1,10 @@
 from needle.cases import NeedleTestCase
+import os
 
 class MyTests(NeedleTestCase):
     engine_class = 'needle.engines.perceptualdiff_engine.Engine'
     
     def test_masthead(self):
         self.driver.implicitly_wait(10)
-        self.driver.get('http://www.bbc.co.uk/news')
+        self.driver.get(os.environ.get('NEEDLE_URL'))
         self.assertScreenshot('#blq-mast', 'bbc-masthead')
