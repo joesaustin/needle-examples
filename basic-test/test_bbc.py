b9c756655355e85429fa02c5148eacb8723165df
Russell Bradley <me@russellbradley.com>
2014-11-09 23:46:09 -0500

    - fix improper module import order

--- basic-test/test_bbc.py
+++ basic-test/test_bbc.py
@@ -1,5 +1,5 @@
-from needle.cases import NeedleTestCase
 import os
+from needle.cases import NeedleTestCase
 
 class MyTests(NeedleTestCase):
     engine_class = 'needle.engines.perceptualdiff_engine.Engine'
